c5516acf90029cdb35592e96d5d9efa723392e26
OsziBen <osziben2@gmail.com>
2025-03-02 21:25:48 +0100

    Migrate database

--- Assignments/CourseController létrehozása/CourseController/CourseController/Migrations/20250302201151_AddAgeToUser.Designer.cs
+++ Assignments/CourseController létrehozása/CourseController/CourseController/Migrations/20250302201151_AddAgeToUser.Designer.cs
@@ -2,6 +2,7 @@
 using CourseController.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CourseController.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250302201151_AddAgeToUser")]
+    partial class AddAgeToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.13");
---
+++ Assignments/CourseController létrehozása/CourseController/CourseController/Migrations/20250302201151_AddAgeToUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CourseController.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAgeToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Users");
+        }
+    }
+}
