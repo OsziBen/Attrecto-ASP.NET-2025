879c4727025ef9d66c4288e5546da640b5e9615a
OsziBen <osziben2@gmail.com>
2025-03-09 19:24:28 +0100

    Migrate database changes

---
+++ Assignments/CourseController létrehozása/CourseController/CourseController/Migrations/20250309175614_FixCourseUserRelationship.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CourseController.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCourseUserRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Users",
+                newName: "Role");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "Courses",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "CourseParticipants",
+                columns: table => new
+                {
+                    CoursesId = table.Column<int>(type: "INTEGER", nullable: false),
+                    UsersId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseParticipants", x => new { x.CoursesId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_CourseParticipants_Courses_CoursesId",
+                        column: x => x.CoursesId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CourseParticipants_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_AuthorId",
+                table: "Courses",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseParticipants_UsersId",
+                table: "CourseParticipants",
+                column: "UsersId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Users_AuthorId",
+                table: "Courses",
+                column: "AuthorId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Users_AuthorId",
+                table: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "CourseParticipants");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_AuthorId",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Courses");
+
+            migrationBuilder.RenameColumn(
+                name: "Role",
+                table: "Users",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "TEXT",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
