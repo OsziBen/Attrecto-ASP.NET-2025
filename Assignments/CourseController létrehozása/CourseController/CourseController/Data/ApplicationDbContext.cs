0bcc0bea4dd0690edceb7f7316fb354022399883
OsziBen <osziben2@gmail.com>
2025-03-10 18:01:17 +0100

    Add model creation rule for foreign key

--- Assignments/CourseController létrehozása/CourseController/CourseController/Data/ApplicationDbContext.cs
+++ Assignments/CourseController létrehozása/CourseController/CourseController/Data/ApplicationDbContext.cs
@@ -8,17 +8,19 @@ namespace CourseController.Data
 
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-            //var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            //DbPath = Path.Join(path, "academyAssignment.db");
         }
-
-        /*
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite($"Data Source={DbPath}");
-        }
-        */
         public DbSet<User> Users { get; set; }
         public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Course>()
+                .HasOne(c => c.Author)
+                .WithMany()
+                .HasForeignKey(c => c.AuthorId)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
     }
 }
