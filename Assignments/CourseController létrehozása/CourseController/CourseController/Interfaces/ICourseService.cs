639fdad1b2694e352be562870c72de92d15c15e4
OsziBen <osziben2@gmail.com>
2025-03-20 14:53:23 +0100

    Create interface for service layer methods

---
+++ Assignments/CourseController létrehozása/CourseController/CourseController/Interfaces/ICourseService.cs
@@ -0,0 +1,13 @@
+﻿using CourseController.Dtos;
+
+namespace CourseController.Interfaces
+{
+    public interface ICourseService
+    {
+        Task CreateAsync(CourseDto data);
+        Task<bool> DeleteAsync(int id);
+        Task<List<CourseDto>> GetAllAsync();
+        Task<CourseDto?> GetByIdAsync(int id);
+        Task<CourseDto?> UpdateAsync(int id, CourseDto data);
+    }
+}
---
+++ Assignments/CourseController létrehozása/CourseController/CourseController/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using CourseController.Dtos;
+
+namespace CourseController.Interfaces
+{
+    public interface IUserService
+    {
+        Task CreateAsync(UserDto data);
+        Task<bool> DeleteAsync(int id);
+        Task<List<UserDto>> GetAllAsync();
+        Task<List<UserDto>> GetAllAdultUsersAsync();
+        Task<UserDto?> GetByIdAsync(int id);
+        Task<UserDto?> UpdateAsync(int id, UserDto data);
+    }
+}
