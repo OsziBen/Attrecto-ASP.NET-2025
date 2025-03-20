e44c4e20393d82994df10a9ebe862590b5e7a36a
OsziBen <osziben2@gmail.com>
2025-03-20 14:51:49 +0100

    Create DTO for data model

---
+++ Assignments/CourseController létrehozása/CourseController/CourseController/Dtos/CourseDto.cs
@@ -0,0 +1,23 @@
+﻿using CourseController.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourseController.Dtos
+{
+    public class CourseDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        public int AuthorId { get; set; }
+
+        public UserDto? Author { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; } = string.Empty;
+    }
+}
