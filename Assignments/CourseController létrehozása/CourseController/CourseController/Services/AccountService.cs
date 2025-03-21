4500f387ec2c8295466fc1461111011946d29c41
OsziBen <osziben2@gmail.com>
2025-03-21 16:45:36 +0100

    Implement login logic handler method

---
+++ Assignments/CourseController létrehozása/CourseController/CourseController/Services/AccountService.cs
@@ -0,0 +1,30 @@
+﻿using CourseController.Data;
+using CourseController.Dtos;
+using CourseController.Interfaces;
+
+namespace CourseController.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AccountService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<User?> LoginAsync(LoginDto loginDto)
+        {
+            var user = await _userRepository.GetByEmailAsync(loginDto.Email);
+
+            if (user != null && user.Password == loginDto.Password)
+            {
+                return user;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
