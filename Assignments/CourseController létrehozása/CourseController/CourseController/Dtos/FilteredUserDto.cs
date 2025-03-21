193c376c45ab9f9a9cbb470be38c1788e923c4f7
OsziBen <osziben2@gmail.com>
2025-03-21 22:40:53 +0100

    Add FilteredUserDto to exclude sensitive user data

---
+++ Assignments/CourseController létrehozása/CourseController/CourseController/Dtos/FilteredUserDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseController.Dtos
+{
+    public class FilteredUserDto
+    {
+        [Required]
+        [StringLength(120)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Role { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0, 150, ErrorMessage = "Age must be between 0 and 150")]
+        public int Age { get; set; }
+    }
+}
