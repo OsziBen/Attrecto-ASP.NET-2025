e38e9e84726c8e13783514cf6a3d37e74ae6eb96
OsziBen <osziben2@gmail.com>
2025-03-21 23:04:23 +0100

    Add new filtered Course DTO

---
+++ Assignments/CourseController létrehozása/CourseController/CourseController/Dtos/FilteredCourseDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseController.Dtos
+{
+    public class FilteredCourseDto
+    {
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        public FilteredUserDto? Author { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; } = string.Empty;
+    }
+}
